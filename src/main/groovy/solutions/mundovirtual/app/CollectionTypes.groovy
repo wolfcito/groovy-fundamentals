56391ab52ca07b5eec5f3ff6eccdd34633f47adf
Luis Fernando Ushiña <guffenix@gmail.com>
2022-04-04 02:42:12 -0500

    maps and sets groovy

--- src/main/groovy/solutions/mundovirtual/app/CollectionTypes.groovy
+++ src/main/groovy/solutions/mundovirtual/app/CollectionTypes.groovy
@@ -18,3 +18,24 @@ aListOf.each {println(it)}
 aListOf.reverseEach {println(it)}
 
 aListOf.eachPermutation {println(it)}
+
+
+//work with Set
+aSetList =['Java', 'Python', 'javascript'] as Set
+println(aSetList)
+println(aSetList.class)
+
+//work with Maps
+aMap = [:]
+println(aMap)
+
+aMap = [courseName:'Gradle', rating: 10, price:100.0]
+println(aMap)
+
+aMap.each {k,v -> println("Key: ${k} , Value: ${v}")}
+println(aMap.courseName)
+println(aMap['courseName'])
+println(aMap.get('courseName'))
+
+aMap.newKey = "It's amazing :)"
+println(aMap.get('newKey'))
